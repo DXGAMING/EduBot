338c9996300211213cdc5a72c4397509ccdf22b1
DXGAMING <42089865+DXGAMING@users.noreply.github.com>
2020-02-16 15:56:53 +0300

    Update UserDAO.java

--- DAO/UserDAO.java
+++ DAO/UserDAO.java
@@ -7,13 +7,6 @@ import org.hibernate.Transaction;
 public class UserDAO implements DBApi<JediBotUser,Long> {
 
     @Override
-    public JediBotUser findById(long id) {
-        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(JediBotUser.class, id);
-
-    }
-    
-
-    @Override
     public JediBotUser findById(Long id) {
         return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(JediBotUser.class, id);
     }
